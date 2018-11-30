7ebfb0d8d7674f6ffdd6c4b6c125a964e89e33f9
Dario Molina <dariomolina93>
2018-11-30 01:39:01 -0800

    Initial commit.  Request permissions,select contacts, and send sms to contacts.

---
+++ app/src/main/java/com/example/dariomolina/alerta/MainActivity.java
@@ -0,0 +1,214 @@
+package com.example.dariomolina.alerta;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button notify;
+    private final int REQUEST_ID_MULTIPLE_PERMISSIONS = 1;
+    private final int REQUEST_CODE_PICK_CONTACT = 2;
+    HashMap<String, Integer> perms;
+    SMS message;
+    private final String HASCONTACTS = "hasContacts";
+    public static final String SHAREDPREFERENCENAME = "ContactNameAndNumbers";
+    SharedPreferences sharedPreference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        perms = new HashMap<>();
+        perms.put(Manifest.permission.SEND_SMS, PackageManager.PERMISSION_GRANTED);
+        perms.put(Manifest.permission.READ_CONTACTS, PackageManager.PERMISSION_GRANTED);
+        checkAndRequestPermissions();
+        message = new SMS(getBaseContext());
+        message.registerReceivers();
+        notify = findViewById(R.id.notify);
+        sharedPreference = getSharedPreferences(SHAREDPREFERENCENAME, MODE_PRIVATE);
+        sharedPreference.edit().putBoolean(HASCONTACTS,false).apply();
+
+        checkAndRequestPermissions();
+
+        if(!sharedPreference.getBoolean(HASCONTACTS, false))
+            selectContacts();
+
+        notify.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String sms = "We can now send messages that are > 160 chars! This gives us greater flexibility in the" +
+                        " content of the message and not be restricted anymore by the previous max limit of 160 chars" +
+                        " per message. The max number of chars that is allowed to send in an sms is 918." +
+                        "  If a message is > 160 chars it will break down the message into batches of 158 chars, and the last" +
+                        " batch being how ever many chars are left. If a message is <= 160, it will only send that one single batch.";
+                Log.d("notifyRelatiesEvent", "Sending Text Message");
+
+
+                Map<String,?> keys = sharedPreference.getAll();
+
+                for(Map.Entry<String,?> entry : keys.entrySet()){
+
+                    String value = entry.getValue().toString();
+                    if(entry.getKey().toString().equals(HASCONTACTS))
+                        continue;
+
+                    message.sendSMS(entry.getKey().toString(), sms);
+                }
+            }
+        });
+    }
+
+    private  void checkAndRequestPermissions() {
+        Log.d("check&RequestPermission", "checking the state of each permission.");
+        int smsPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS);
+        int contactsPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS);
+
+        ArrayList<String> permissionsNeeded = new ArrayList<>();
+        if (contactsPermission != PackageManager.PERMISSION_GRANTED)
+            permissionsNeeded.add(Manifest.permission.READ_CONTACTS);
+
+        if (smsPermission != PackageManager.PERMISSION_GRANTED)
+            permissionsNeeded.add(Manifest.permission.SEND_SMS);
+
+        if (!permissionsNeeded.isEmpty()) {
+            Log.d("check&RequestPermission", "requesting permission");
+            ActivityCompat.requestPermissions(this, permissionsNeeded.toArray(new String[permissionsNeeded.size()]), REQUEST_ID_MULTIPLE_PERMISSIONS);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_ID_MULTIPLE_PERMISSIONS: {
+                if (grantResults.length > 0) {
+                    Log.d("RequestPermissionResult", "grantResults array contains elements.");
+                    for (int i = 0; i < permissions.length; i++)
+                        perms.put(permissions[i], grantResults[i]);
+
+                    if (perms.get(Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED
+                            || perms.get(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+                        Log.d("RequestPermissionResult", "One or both permissions are not granted.");
+
+                        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.SEND_SMS) || ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CONTACTS)) {
+                            Log.d("RequestPermissionResult", "Displaying alert dialog for requesting permissions.");
+                            showDialogOK("Permiso para leer contactos y mandar mensages son necesarios para utilizar la aplicacion.",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            switch (which) {
+                                                case DialogInterface.BUTTON_POSITIVE:
+                                                    checkAndRequestPermissions();
+                                                    break;
+                                            }
+                                        }
+                                    });
+                        }
+                        else {
+                            Log.d("RequestPermissionResult", "Opening app setttings to manually enable permissions.");
+                            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                            Uri uri = Uri.fromParts("package", getPackageName(), null);
+                            intent.setData(uri);
+                            startActivityForResult(intent, REQUEST_ID_MULTIPLE_PERMISSIONS);
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    private void showDialogOK(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .create()
+                .show();
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        message.unRegisterReceivers();
+        super.onDestroy();
+    }
+
+    private void selectContacts() {
+        Intent phonebookIntent = new Intent("intent.action.INTERACTION_TOPMENU");
+        phonebookIntent.putExtra("additional", "phone-multi");
+        phonebookIntent.putExtra("FromMMS", true);
+        startActivityForResult(phonebookIntent, REQUEST_CODE_PICK_CONTACT);
+    }
+
+    private HashMap<String,String> getAllContacts()
+    {
+        HashMap<String,String> nameAndPhone = new HashMap<>();
+//        String[] columns = {ContactsContract.Contacts._ID, ContactsContract.Contacts.DISPLAY_NAME, ContactsContract.Contacts.HAS_PHONE_NUMBER};
+//        Cursor cursor = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI,columns, null, null, null);
+//        while (cursor.moveToNext()) {
+//            String contactId = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+//            String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+//            nameID.put(contactId,name);
+//        }
+//
+//        cursor.close();
+//        return nameID;
+
+
+        Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,null,null, null);
+        while (phones.moveToNext())
+        {
+            String name=phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+            String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+            phoneNumber =  phoneNumber.replaceAll("[^0-9]", "");
+            nameAndPhone.put(phoneNumber,name);
+        }
+        phones.close();
+        return nameAndPhone;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode==RESULT_OK)
+        {
+            if(requestCode == REQUEST_CODE_PICK_CONTACT  )
+            {
+                SharedPreferences.Editor editor = sharedPreference.edit();
+                Bundle bundle =  data.getExtras();
+                ArrayList<String> selectedContacts = bundle.getStringArrayList("result");
+                sharedPreference.edit().putBoolean(HASCONTACTS,true).apply();
+
+                HashMap<String,String> allContacts = getAllContacts();
+                for(int i =0; i < selectedContacts.size(); i++)
+                {
+                    String[] results = selectedContacts.get(i).split(";");
+                    String phoneNumber = results[1].replaceAll("[^0-9]", "");
+                    if(allContacts.get(phoneNumber) != null)
+                        editor.putString(phoneNumber,allContacts.get(phoneNumber));
+                }
+
+                editor.apply();
+                super.onActivityResult(requestCode,resultCode,data);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/dariomolina/alerta/SMS.java
@@ -0,0 +1,115 @@
+package com.example.dariomolina.alerta;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/*
+    Created by: Dario Molina
+    Date: 11/22/2018
+ */
+
+public class SMS
+{
+
+    private Context applicationContext;
+    private BroadcastReceiver messageSendReceiver, messageDeliveredReceiver;
+    private final String SEND = "SMS_SEND";
+    private final String DELIVERED = "SMS_DELIVERED";
+    private static final int SMS_MAX_LENGTH = 160;
+
+    public SMS(Context context)
+    {
+        applicationContext = context;
+        messageSendReceiver = null;
+        messageDeliveredReceiver = null;
+    }
+
+    public void registerReceivers()
+    {
+        messageSendReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                switch (getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        Log.d("registerRecievers", "SMS has been sent.");
+                        Toast.makeText(applicationContext, "Mensaje de texto a sido enviado.", Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        Log.d("registerRecievers", "Generic Test failure. SMS not send.");
+                        Toast.makeText(applicationContext, "Mensaje de texto no fue enviado. Porfavor intente de nuevo.", Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        Log.d("registerRecievers", "No Service.  SMS not send.");
+                        Toast.makeText(applicationContext, "Sin Servicio, mensaje de Texto no fue enviado.", Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        Log.d("registerRecievers", "Null PDU. SMS not send.");
+                        Toast.makeText(applicationContext, "Mensaje de texto no fue enviado.  Porfavor intente de nuevo.", Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        Log.d("registerRecievers", "Error Radio off");
+                        Toast.makeText(applicationContext, "Sin coneccion a wifi. Si esta conectado," +
+                                " asegurese que pueda mandar mensajes por wifi.", Toast.LENGTH_LONG).show();
+                        break;
+                }
+            }
+        };
+
+        messageDeliveredReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                switch (getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        Log.d("registerRecievers", "SMS delivered");
+                        Toast.makeText(applicationContext, "Su contacto a recibido su mensaje de texto.", Toast.LENGTH_SHORT).show();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Log.d("registerRecievers", "SMS not delivered");
+                        Toast.makeText(applicationContext, "Su contacto no pudo recibir su mensaje de texto.", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        };
+
+        applicationContext.registerReceiver(messageSendReceiver, new IntentFilter(SEND));
+        applicationContext.registerReceiver(messageDeliveredReceiver , new IntentFilter(DELIVERED));
+    }
+
+    public void sendSMS(String phoneNumber, String message)
+    {
+        Log.d("sendSMS", "registering pending intents for sms send and  sms delivered.");
+        PendingIntent sentPI = PendingIntent.getBroadcast(applicationContext, 0, new Intent(SEND), 0);
+        PendingIntent deliveredPI = PendingIntent.getBroadcast(applicationContext, 0, new Intent(DELIVERED), 0);
+        SmsManager smsManager = SmsManager.getDefault();
+
+        if(message.length() > SMS_MAX_LENGTH) {
+            Log.d("sendSMS", "sms > 160 chars.");
+            smsManager.sendMultipartTextMessage(phoneNumber, null, smsManager.divideMessage(message), new ArrayList<>(Arrays.asList(sentPI)), new ArrayList<>(Arrays.asList(deliveredPI)));
+        }
+        else {
+            Log.d("sendSMS", "sms <= 160 chars.");
+            smsManager.sendTextMessage(phoneNumber, null, message, sentPI, deliveredPI);
+        }
+    }
+
+    public void unRegisterReceivers()
+    {
+        Log.d("unRegisterReceivers", "unregistering receivers for sms.");
+        applicationContext.unregisterReceiver(messageSendReceiver);
+        applicationContext.unregisterReceiver(messageDeliveredReceiver);
+    }
+
+}
+
