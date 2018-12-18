916216d9bcdc3f59fd0037b6255ee6b1fba68de0
Dario Molina <dariomolina93>
2018-12-18 03:13:08 -0800

    Added adds to app, and added functionality to swipe or click on tabs for the different sections.

--- app/src/main/java/com/example/dariomolina/alerta/MainActivity.java
+++ app/src/main/java/com/example/dariomolina/alerta/MainActivity.java
@@ -1,52 +1,22 @@
 package com.example.dariomolina.alerta;
 
-import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 
-import java.io.Serializable;
-import java.util.Map;
-
-public class MainActivity extends AppCompatActivity{
-
-    Button notify;
-    SMS message;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        message = new SMS(this);
-        message.registerReceivers();
-        notify = findViewById(R.id.notify);
 
-        notify.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                String sms = "Testing activities.";
-                Log.d("notifyEvent", "Sending Text Message");
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(new PageAdapter(getSupportFragmentManager()));
 
-                SharedPreferences sharedPreference = getApplicationContext().getSharedPreferences("ContactNameAndNumbers", MODE_PRIVATE);
-                Map<String,?> keys = sharedPreference.getAll();
-                int i = 0;
-                for(Map.Entry<String,?> entry : keys.entrySet()){
-
-                    String name = entry.getValue().toString();
-                    String phone = entry.getKey();
-                    message.sendSMS(phone, sms, name, i);
-                    i++;
-                }
-            }
-        });
-    }
-
-    @Override
-    protected void onDestroy()
-    {
-        message.unRegisterReceivers();
-        super.onDestroy();
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
